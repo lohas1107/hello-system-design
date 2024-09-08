038899015949e1ccc1684fb4ee11c668c2798b2f
Irene Wang <lohas1107@gmail.com>
2024-09-08 08:25:43 +0800

    test: pass case authenticated_api_request_count_below_limit

--- system/e2e/rate_limiter_test.go
+++ system/e2e/rate_limiter_test.go
@@ -1,7 +1,10 @@
 package e2e
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/steinfletcher/apitest"
 	"github.com/stretchr/testify/suite"
+	"net/http"
 	"testing"
 )
 
@@ -17,11 +20,27 @@ func (s *RateLimiterTestSuite) SetupTest() {
 
 }
 
+func Router() *gin.Engine {
+	router := gin.Default()
+	v1 := router.Group("v1")
+	v1.POST("/orders", CreateOrder)
+	return router
+}
+
+func CreateOrder(context *gin.Context) {
+	context.Status(http.StatusCreated)
+}
+
+func (s *RateLimiterTestSuite) Test_authenticated_api_request_count_below_limit() {
+	apitest.New().Debug().
+		Handler(Router()).
+		Post("http://localhost:8080/v1/orders").
+		Expect(s.T()).
+		Status(http.StatusCreated).
+		End()
+}
+
 func (s *RateLimiterTestSuite) Test_api_request_count_below_limit() {
-	s.T().Run("authenticated", func(t *testing.T) {
-
-	})
-
 	s.T().Run("unauthenticated", func(t *testing.T) {
 
 	})
